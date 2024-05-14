1a27acdb7ec8a3c4cf68321d4a8db35000b39bcc
JAMESKirkQI <96830555+JAMESKirkQI@users.noreply.github.com>
2024-05-14 19:33:40 +0800

    Update argpaser.py

--- argpaser.py
+++ argpaser.py
@@ -21,7 +21,7 @@ def argparse_option():
     parser.add_argument('--GDC', action='store_true', default=True, help='RECT loss function')
     parser.add_argument('--lamb', type=float, default=1, help='coefficient for the RECT loss function')
     parser.add_argument('--sigma', type=float, default=1, help='parameter of Gaussian Kernel')
-    parser.add_argument('--sigmaDG', type=float, default=1, help='parameter of Gaussian Kernel in diffusion')
+    parser.add_argument('--sigmaGD', type=float, default=1, help='parameter of Gaussian Kernel in diffusion')
     parser.add_argument('--alpha', type=float, default=0.9, help='parameter of label propagation')
     parser.add_argument('--alphaGDC', type=float, default=0.9, help='parameter of Graph diffusion')
     parser.add_argument('--nearest', type=int, default=3, help='K nearest points')
